0e11d156acb44526bccd775ec416e13bdebcebec
Enzo Figlioli <enzofiglioli.p@gmail.com>
2025-01-15 12:58:13 -0300

    Cambios y consumo de api

--- src/components/CategoriaContainer.jsx
+++ src/components/CategoriaContainer.jsx
@@ -1,26 +1,49 @@
+import { useEffect, useState } from "react";
+import { baseDir } from "../path";
+
 const CategoriaContainer = () => {
+  const [categorias, setCategorias] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`${baseDir}/api/categorias`)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res)
+        setCategorias(res);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
     <nav aria-label="Categorías">
-  <div className="my-2 border-l-4 border-gray-300 dark:border-gray-700 pl-4">
-    <h2 className="text-2xl font-semibold text-white mb-4">Categorías</h2>
-    <ul className="space-y-2">
-      <li className="text-white">
-        <a href="#" className="hover:underline">Política</a>
-      </li>
-      <li className="text-white">
-        <a href="#" className="hover:underline">Economía</a>
-      </li>
-      <li className="text-white">
-        <a href="#" className="hover:underline">Deportes</a>
-      </li>
-      <li className="text-white">
-        <a href="#" className="hover:underline">IRL</a>
-      </li>
-    </ul>
-  </div>
-</nav>
-
-  )
-}
+      <div className="my-2 border-l-4 border-gray-300 dark:border-gray-700 pl-4">
+        <h2 className="text-2xl font-semibold text-white mb-4">Categorías</h2>
+        <ul className="space-y-2">
+          {categorias.map((i, ix) => (
+            <li className="text-white" key={ix}>
+              <a href="#" className="hover:underline">
+                {i.nombre}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default CategoriaContainer;
--- src/components/MensajesTablero.jsx
+++ src/components/MensajesTablero.jsx
@@ -3,26 +3,40 @@ import Tweet from "./Tweet";
 import InputMensaje from "./InputMensaje.jsx";
 import { handlerMessage } from "../handler/manejadorMensajes";
 import data from "../data/data.json";
-import {useSession} from "../context/SessionContext.jsx"
+import { useSession } from "../context/SessionContext.jsx";
+import { baseDir } from "../path.js";
 
 const MensajesTablero = () => {
   const [mensajes, setMensajes] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const {session} = useSession();
-
+  const { session } = useSession();
 
   useEffect(() => {
-    handlerMessage(data)
-      .then((data) => {
-        setMensajes(data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        setError("No se pudo cargar los mensajes.");
-        setLoading(false);
-      });
-  }, []);
+    if (session) {
+      fetch(`${baseDir}/api/tweets`,{method:"GET"})
+        .then((res) =>  res.json())
+        .then((res) => {
+          setMensajes(res);
+          setLoading(false);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setLoading(false);
+        });
+    } else {
+      handlerMessage(data)
+        .then((data) => {
+          setMensajes(data);
+          setLoading(false);
+        }
+      )
+        .catch(() => {
+          setError("No se pudo cargar los mensajes.");
+          setLoading(false);
+        });
+    }
+  }, [session]);
 
   const formatearFecha = (fechaMensaje) => {
     const fechaActual = new Date();
@@ -30,21 +44,27 @@ const MensajesTablero = () => {
     const diferencia = fechaActual - fechaMsg;
     const diasDiferencia = diferencia / (1000 * 3600 * 24);
 
-    if(fechaActual.getDate() == fechaMsg.getDate() && fechaActual.getDay() == fechaMsg.getDay &&
-        fechaActual.getFullYear() == fechaMsg.getFullYear()){
-        return "Hoy";
+    if (fechaActual.toDateString() === fechaMsg.toDateString()) {
+      return "Hoy";
     }
 
     if (diasDiferencia < 7) {
       return fechaMsg.toLocaleDateString();
-    }else {
+    } else {
       const semanas = Math.floor(diasDiferencia / 7);
       return `${semanas} semana(s) atrás`;
     }
   };
 
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="loader-container">
+        <img
+          src="https://i.pinimg.com/originals/65/ba/48/65ba488626025cff82f091336fbf94bb.gif"
+          alt="loader"
+        />
+      </div>
+    );
   }
 
   if (error) {
@@ -53,16 +73,11 @@ const MensajesTablero = () => {
 
   return (
     <div className="mt-4">
-      { session ? 
-      (
-        <InputMensaje />
-      ) 
-      : 
-      ("")}
+      {session && <InputMensaje />}
       {mensajes.length > 0 ? (
         mensajes.map((msg, index) => (
           <Tweet
-            key={msg.id || index}
+            key={msg.id || `msg-${index}`}
             contenido={msg.contenido}
             fecha={formatearFecha(msg.fecha)}
             usuario={msg.usuario}
@@ -71,10 +86,7 @@ const MensajesTablero = () => {
           />
         ))
       ) : (
-        <img
-          src="https://i.pinimg.com/originals/65/ba/48/65ba488626025cff82f091336fbf94bb.gif"
-          alt="loader"
-        />
+        <div>No hay mensajes disponibles.</div>
       )}
     </div>
   );
