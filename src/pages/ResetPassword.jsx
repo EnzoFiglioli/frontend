f3d5312c9ce1c1af97e5f03e76d7fc6356da5338
Enzo Figlioli <enzofiglioli.p@gmail.com>
2025-02-04 14:58:32 -0300

    Agregar funcionalidad para restablecimiento de contraseña y actualizar la URL base del servidor a localhost

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,14 +1,32 @@
 import { useState,useEffect } from "react";
 import {ArrowLeft, Sun} from "lucide-react";
+import {baseDir} from "../path.js";
 
 const ResetPassword = () => {
   const [email, setEmail] = useState("");
   const [theme, setTheme] = useState("dark");
 
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Password reset email sent to:", email);
-  };
+
+    fetch(`${baseDir}/api/usuarios/reset-password`,{
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: email
+      })
+    })
+    .then(response => response.json())
+    .then(response => {
+      if(response.status === 200){
+        alert("Se ha enviado un mail a tu correo para cambiar la contraseña");
+      }
+    })
+    .catch((error) => {console.error('Error:', error)});
+  }
 
   useEffect(() => {
     const storedTheme = localStorage.getItem("theme") || "dark";
@@ -34,7 +52,7 @@ const ResetPassword = () => {
         <h2 className="text-2xl font-semibold text-center text-gray-700 mb-6 dark:text-white">
           Introduce tu mail para cambiar tu contraseña
         </h2>
-        <form className="space-y-4" onSubmit={handleSubmit}>
+        <form className="space-y-4" onSubmit={(e)=> handleSubmit(e)}>
           <div>
             <label
               htmlFor="email"
@@ -49,7 +67,7 @@ const ResetPassword = () => {
               placeholder="Aqui va tu mail"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="mt-2 block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+              className="mt-2 block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 dark:text-black"
             />
           </div>
           <div>
